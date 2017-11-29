9db0978ad0bd80938cb52d157651c7504ea143ab
Sander Bouwhuis <sander@enrise.com>
2017-11-29 12:30:37 +0100

    Sorted

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,98 +1,104 @@
-tap "caskroom/cask"
 # tap "homebrew/bundle"
 # tap "kryptco/tap"
 # cask "osxfuse"
 # cask "xquartz"
-brew "bash"
 # brew "cloc"
-brew "coreutils"
 # brew "ctop"
+# brew "grep"
+# brew "hub"
+# brew "kubernetes-cli"
+# brew "lastpass-cli"
+# brew "mitmproxy"
+# brew "ssh-copy-id"
+# brew "tmux"
+# brew "keithbsmiley/formulae/lunchy"
+# brew "keithbsmiley/formulae/zap"
+# cask "android-file-transfer"
+# cask "android-sdk"
+# cask "boxer"
+# cask "dosbox"
+# cask "foldingtext"
+# cask "gpg-suite"
+# cask "hyper"
+# cask "lastpass"
+# cask "minikube"
+# cask "skype"
+# cask "softu2f"
+# cask "sourcetree"
+# cask "sql-tabs"
+# cask "trailer"
+# cask "virtualbox"
+# cask "virtualbox-extension-pack"
+# cask "viscosity"
+# cask "vlc"
+# cask "whatsapp"
+# cask "wireshark"
+# mas "Daily", id: 686910553
+# mas "Flow", id: 469770826
+# mas "iPhoto", id: 408981381
+# mas "Microsoft Remote Desktop", id: 715768417
+# mas "Pocket", id: 568494494
+# mas "Slack", id: 803453959
+# mas "Trello", id: 1278508951
+# mas "TurboCollage", id: 433371445
+# mas "TweetDeck", id: 485812721
+
+tap "caskroom/cask"
+brew "bash"
+brew "coreutils"
 brew "direnv"
 brew "findutils"
 brew "git"
 brew "git-cal"
 brew "git-crypt"
-# brew "grep"
 brew "htop"
 brew "httpstat"
-# brew "hub"
 brew "icdiff"
 brew "jq"
-# brew "kubernetes-cli"
-# brew "lastpass-cli"
 brew "mas"
-# brew "mitmproxy"
 brew "moreutils"
 brew "ncdu"
 brew "peco"
 brew "pv"
 brew "rename"
 brew "ripgrep"
-# brew "ssh-copy-id"
 brew "tig"
-# brew "tmux"
 brew "tree"
 brew "vim"
 brew "watch"
 brew "wifi-password"
 brew "zsh"
-# brew "keithbsmiley/formulae/lunchy"
-# brew "keithbsmiley/formulae/zap"
 cask "airdroid"
 cask "alfred"
-# cask "android-file-transfer"
-# cask "android-sdk"
 cask "appcleaner"
-# cask "boxer"
 cask "dashlane"
-# cask "dosbox"
 cask "dropbox"
 cask "firefox"
-# cask "foldingtext"
 cask "franz"
 cask "google-backup-and-sync"
-# cask "gpg-suite"
-# cask "hyper"
 cask "google-chrome"
 cask "iterm2"
 cask "keybase"
-# cask "lastpass"
-# cask "minikube"
 cask "qlstephen"
 cask "skitch"
-# cask "skype"
-# cask "softu2f"
-# cask "sourcetree"
 cask "spotify"
-# cask "sql-tabs"
 cask "standard-notes"
 cask "steam"
 cask "torbrowser"
-# cask "trailer"
 cask "transmission"
 cask "typinator"
 cask "veracrypt"
-# cask "virtualbox"
-# cask "virtualbox-extension-pack"
-# cask "viscosity"
 cask "visual-studio-code"
-# cask "vlc"
-# cask "whatsapp"
-# cask "wireshark"
 cask "caskroom/drivers/sonos"
 cask "caskroom/drivers/tomtom-sports-connect"
 cask "caskroom/versions/gpg-suite-beta"
 mas "BetterSnapTool", id: 417375580
 mas "Boxy", id: 1053031090
 mas "Canary Mail", id: 1236045954
-# mas "Daily", id: 686910553
 mas "DaisyDisk", id: 411643860
 mas "Dice Pass", id: 997688302
 mas "Fenêtre", id: 1286743037
-# mas "Flow", id: 469770826
 mas "Folder Tidy", id: 486626129
-# mas "iPhoto", id: 408981381
-# mas "Microsoft Remote Desktop", id: 715768417
 mas "GarageBand", id: 682658836
 mas "iMovie", id: 408981434
 mas "Keynote", id: 409183694
@@ -100,9 +106,4 @@ mas "LastPass", id: 926036361
 mas "Numbers", id: 409203825
 mas "Pages", id: 409201541
 mas "PiPifier", id: 1160374471
-# mas "Pocket", id: 568494494
 mas "Shazam", id: 897118787
-# mas "Slack", id: 803453959
-# mas "Trello", id: 1278508951
-# mas "TurboCollage", id: 433371445
-# mas "TweetDeck", id: 485812721
